4f6bcbe00c7fd727c1570c4fc94af4ef77c5d5f8
Daniel <faltynowskidaniel6@gmail.com>
2023-10-15 13:36:45 +0200

    I coded a simple program to create matrix and print it.

--- adjacencyMatrix/main.cpp
+++ adjacencyMatrix/main.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+void printMatrix(vector<vector<int> >& matrix, int length);
+void createAdjacencyMatrix(vector<vector<int> >& adjacencyMatrix, int verticles);
+
 int main()
 {
+    int n = 5;
+    vector<vector<int> > A(n, vector<int>(n));
+    createAdjacencyMatrix(A, n);
+    printMatrix(A, n);
     return 0;
+}
+
+void printMatrix(vector<vector<int> >& matrix, int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < length; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void createAdjacencyMatrix(vector<vector<int> >& adjacencyMatrix, int verticles)
+{
+    for (int i = 0; i < verticles; i++)
+    {
+        for (int j = 0; j < verticles; j++)
+        {
+            adjacencyMatrix[i][j] = 0;
+        }
+    }
 }
