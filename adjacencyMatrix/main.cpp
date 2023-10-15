587440b16ec91242546b2ae5b9935ae9b4568d83
Daniel <faltynowskidaniel6@gmail.com>
2023-10-15 19:04:29 +0200

    I finished my experiments with adjacency matrix in c++.

--- adjacencyMatrix/main.cpp
+++ adjacencyMatrix/main.cpp
@@ -11,24 +11,102 @@ void setEdge(vector<vector<int> >& adjacencyMatrix, bool directed);
 void deleteVerticle(vector<vector<int> >& adjacencyMatrix, int verticle);
 void deleteEdge(vector<vector<int> >& adjacencyMatrix, bool directed);
 void exportGraph(int verticles, vector<vector<int> >& adjacencyMatrix);
+void determineTheVerticlesOutgoingDegree(vector<vector<int> >& adjacencyMatrix, int verticle, vector<int>& results);
+void determineTheVerticlesIncomingDegree(vector<vector<int> >& adjacencyMatrix, int verticle, vector<int>& results);
+int countMinimalVerticle(vector<int>& results, int verticles);
+int countMaximalVerticle(vector<int>& results, int verticles);
+int amountOfEvenVerticles(vector<int>& results, int verticles);
+int amountOfOddVerticles(vector<int>& results, int verticles);
+void showMenu();
+
 
 int main()
 {
     int verticles = setVerticles();
+    bool directed;
+    vector<int> results;
+
+    cout << "Do you want to create a directed graph?" << endl;
+    cout << "\t0) No" << "\n\t1) Yes" << endl << endl;
+    cin >> directed;
+
     vector<vector<int> > A(verticles, vector<int>(verticles));
     createAdjacencyMatrix(A, verticles);
-    printMatrix(A);
 
-    setEdge(A, false);
-    setEdge(A, false);
-    setEdge(A, false);
-    setEdge(A, false);
-    setEdge(A, false);
-    setEdge(A, false);
-    setEdge(A, false);
-    setEdge(A, false);
-    setEdge(A, false);
-    exportGraph(verticles, A);
+    while(true)
+    {
+        showMenu();
+        int option;
+        cin >> option;
+
+        switch(option)
+        {
+            case 1:
+                printMatrix(A);
+                break;
+            case 2:
+                break;
+            case 3:
+                setEdge(A, directed);
+                break;
+            case 4:
+                deleteVerticle(A, verticles);
+                break;
+            case 5:
+                deleteEdge(A, directed);
+                break;
+            case 6:
+                exportGraph(verticles, A);
+                break;
+            case 7:
+                cout << "Statictics of outgoing verticles!" << endl;
+                cout << "\tDegrees:\n";
+                determineTheVerticlesOutgoingDegree(A, verticles, results);
+                for (int i = 0; i < verticles; i++)
+                {
+                    cout << "\t\tdeg(" << i << ") = "
+                         << "\t\t" << results[i] << endl;
+                }
+                cout << endl;
+                cout << "\tMinimal degree: " << countMinimalVerticle(results, verticles) << endl;
+                cout << "\tMaximal degree: " << countMaximalVerticle(results, verticles) << endl;
+                cout << "\tAmount of even verticles: " << amountOfEvenVerticles(results, verticles) << endl;
+                cout << "\tAmount of odd verticles: " << amountOfOddVerticles(results, verticles) << endl;
+                std::sort(results.begin(), results.end());
+                cout << "\tSequence of degrees: <";
+                for (int i = 0; i < verticles; i++)
+                {
+                    cout << results[i] << " ";
+                }
+                cout << ">" << endl << endl;
+
+                cout << "Statictics of incoming verticles!" << endl;
+                cout << "\tDegrees:";
+                determineTheVerticlesIncomingDegree(A, verticles, results);
+                for (int i = 0; i < verticles; i++)
+                {
+                    cout << "\t\tdeg(" << i << ") = "
+                         << "\t\t" << results[i] << endl;
+                }
+                cout << endl;
+                cout << "\tMinimal degree: " << countMinimalVerticle(results, verticles) << endl;
+                cout << "\tMaximal degree: " << countMaximalVerticle(results, verticles) << endl;
+                cout << "\tAmount of even verticles: " << amountOfEvenVerticles(results, verticles) << endl;
+                cout << "\tAmount of odd verticles: " << amountOfOddVerticles(results, verticles) << endl;
+                std::sort(results.begin(), results.end());
+                cout << "\tSequence of degrees: <";
+                for (int i = 0; i < verticles; i++)
+                {
+                    cout << results[i] << " ";
+                }
+                cout << ">" << endl << endl;
+                break;
+            case 8:
+                return 0;
+            default:
+                cout << "You prompted an incorrect number!" << endl << endl;
+        }
+    }
 
     return 0;
 }
@@ -143,4 +221,86 @@ void exportGraph(int verticles, vector<vector<int> >& adjacencyMatrix)
         }
         FileEdges.close();
     }
+}
+
+void determineTheVerticlesOutgoingDegree(vector<vector<int> >& adjacencyMatrix, int verticle, vector<int>& results)
+{
+    results.clear();
+    for (int i = 0; i < verticle; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < verticle; j++)
+        {
+            sum = sum + adjacencyMatrix[i][j];
+        }
+        results.push_back(sum);
+    }
+}
+
+void determineTheVerticlesIncomingDegree(vector<vector<int> >& adjacencyMatrix, int verticle, vector<int>& results)
+{
+    results.clear();
+    for (int i = 0; i < verticle; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < verticle; j++)
+        {
+            sum = sum + adjacencyMatrix[j][i];
+        }
+        results.push_back(sum);
+    }
+}
+
+int countMinimalVerticle(vector<int>& results, int verticles)
+{
+    sort(results.begin(), results.end());
+    return results[0];
+}
+
+int countMaximalVerticle(vector<int>& results, int verticles)
+{
+    sort(results.begin(), results.end());
+    return results[verticles - 1];
+}
+
+int amountOfEvenVerticles(vector<int>& results, int verticles)
+{
+    int result = 0;
+
+    for (int i = 0; i < verticles; i++)
+    {
+        if (results[i] % 2 == 0)
+        {
+            result = result + 1;
+        }
+    }
+    return result;
+}
+
+int amountOfOddVerticles(vector<int>& results, int verticles)
+{
+    int result = 0;
+
+    for (int i = 0; i < verticles; i++)
+    {
+        if (results[i] % 2 != 0)
+        {
+            result = result + 1;
+        }
+    }
+    return result;
+}
+
+void showMenu()
+{
+    cout << "----------------------------------------" << endl;
+    cout << "Select an option:"
+         << "\n\t1) Print adjancency matrix."
+         << "\n\t2) Add a verticle."
+         << "\n\t3) Add an edge"
+         << "\n\t4) Delete a verticle."
+         << "\n\t5) Delete an edge."
+         << "\n\t6) Export graph"
+         << "\n\t7) Show statistics."
+         << "\n\t8) Exit." << endl << endl;
 }
--- adjacencyMatrix/print_graph.py
+++ adjacencyMatrix/print_graph.py
@@ -1,35 +1,53 @@
+import os
 import networkx
 import matplotlib.pyplot as plt
 
-with open('verticles.txt', "r") as FileVerticles:
-    verticles = int(FileVerticles.readline())
-    FileVerticles.close()
+
+def import_number_of_verticles():
+    with open('verticles.txt', "r") as FileVerticles:
+        verticles = int(FileVerticles.readline())
+        FileVerticles.close()
+    return verticles
 
 
-with open('edges.txt', "r") as FileEdges:
-    data = []
-    for datum in FileEdges:
-        data.append(int(datum))
+def import_edges():
+    with open('edges.txt', "r") as FileEdges:
+        data = []
+        for datum in FileEdges:
+            data.append(int(datum))
+        adjacency_matrix = []
+        FileEdges.close()
+    return data
+
+
+def create_adjacency_matrix(data, verticles):
     adjacency_matrix = []
-    FileEdges.close()
+    for i in range(verticles):
+        adjacency_matrix.append([])
+    for i in range(verticles):
+        for j in range(verticles):
+            adjacency_matrix[i].append(data[i * verticles + j])
+    return adjacency_matrix
 
 
-for i in range(verticles):
-    adjacency_matrix.append([])
-for i in range(verticles):
-    for j in range(verticles):
-        adjacency_matrix[i].append(data[i * verticles + j])
+def create_list_of_edges(adjacency_matrix, verticles):
+    edges = []
+    for i in range(verticles):
+        for j in range(verticles):
+            if adjacency_matrix[i][j] != 0:
+                for value in range(adjacency_matrix[i][j]):
+                    edges.append((i, j))
+    return edges
 
 
-print(adjacency_matrix)
-edges = []
-for i in range(verticles):
-    for j in range(verticles):
-        if adjacency_matrix[i][j] != 0:
-            for value in range(adjacency_matrix[i][j]):
-                edges.append((i, j))
-
-
-G = networkx.Graph(edges)
-networkx.draw(G)
-plt.show()
+if __name__ == "__main__":
+    if os.path.exists('verticles.txt') and os.path.exists('edges.txt'):
+        n = import_number_of_verticles()
+        edges = import_edges()
+        A = create_adjacency_matrix(edges, n)
+        edges = create_list_of_edges(A, n)
+        G = networkx.Graph(edges)
+        networkx.draw(G)
+        plt.show()
+    else:
+        print("There is no data to read. First, run the main.cpp program.")
