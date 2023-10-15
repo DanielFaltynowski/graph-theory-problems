5a0c793059648924bef4b9191e0d3d70ed6da699
Daniel <faltynowskidaniel6@gmail.com>
2023-10-15 15:11:46 +0200

    I implemented functions to add or delete vertexes and edges.

--- adjacencyMatrix/main.cpp
+++ adjacencyMatrix/main.cpp
@@ -4,28 +4,39 @@
 
 using namespace std;
 
-void printMatrix(vector<vector<int> >& matrix, int length);
+void printMatrix(vector<vector<int> >& matrix);
 void createAdjacencyMatrix(vector<vector<int> >& adjacencyMatrix, int verticles);
 int setVerticles();
 void setEdge(vector<vector<int> >& adjacencyMatrix, bool directed);
+void deleteVerticle(vector<vector<int> >& adjacencyMatrix, int verticle);
+void deleteEdge(vector<vector<int> >& adjacencyMatrix, bool directed);
 
 int main()
 {
     int verticles = setVerticles();
     vector<vector<int> > A(verticles, vector<int>(verticles));
     createAdjacencyMatrix(A, verticles);
-    printMatrix(A, verticles);
+    printMatrix(A);
     setEdge(A, false);
-    printMatrix(A, verticles);
+    printMatrix(A);
+    setEdge(A, false);
+    printMatrix(A);
+    setEdge(A, false);
+    printMatrix(A);
+    cout << endl;
+    deleteVerticle(A, 0);
+    printMatrix(A);
+    deleteEdge(A, false);
+    printMatrix(A);
 
     return 0;
 }
 
-void printMatrix(vector<vector<int> >& matrix, int length)
+void printMatrix(vector<vector<int> >& matrix)
 {
-    for (int i = 0; i < length; i++)
+    for (int i = 0; i < matrix.size(); i++)
     {
-        for (int j = 0; j < length; j++)
+        for (int j = 0; j < matrix.size(); j++)
         {
             cout << matrix[i][j] << " ";
         }
@@ -47,15 +58,15 @@ void createAdjacencyMatrix(vector<vector<int> >& adjacencyMatrix, int verticles)
 int setVerticles()
 {
     int verticles;
-    ofstream File("verticles.txt");
+//    ofstream File("verticles.txt");
 
     cout << "Enter the number of vertices: " << endl;
     cin >> verticles;
-    if (File.is_open())
-    {
-        File << verticles;
-        File.close();
-    }
+//    if (File.is_open())
+//    {
+//        File << verticles;
+//        File.close();
+//    }
 
     return verticles;
 }
@@ -76,5 +87,40 @@ void setEdge(vector<vector<int> >& adjacencyMatrix, bool directed)
     if (!directed)
     {
         adjacencyMatrix[in][out] = adjacencyMatrix[in][out] + 1;
+    }
+}
+
+void deleteVerticle(vector<vector<int> >& adjacencyMatrix, int verticle)
+{
+    adjacencyMatrix.erase(adjacencyMatrix.begin() + verticle);
+
+    for (int i = 0; i < adjacencyMatrix.size(); i++)
+    {
+        adjacencyMatrix[i].erase(adjacencyMatrix[i].begin() + verticle);
+    }
+}
+
+void deleteEdge(vector<vector<int> >& adjacencyMatrix, bool directed)
+{
+    int out;
+    int in;
+
+    cout << "Enter an outgoing vertex: ";
+    cin >> out;
+
+    cout << "Enter an incoming vertex: ";
+    cin >> in;
+
+    if (adjacencyMatrix[out][in] > 0)
+    {
+        if (!directed && adjacencyMatrix[in][out] > 0)
+        {
+            adjacencyMatrix[out][in] = adjacencyMatrix[out][in] - 1;
+            adjacencyMatrix[in][out] = adjacencyMatrix[in][out] - 1;
+        }
+        else
+        {
+            adjacencyMatrix[out][in] = adjacencyMatrix[out][in] - 1;
+        }
     }
 }
